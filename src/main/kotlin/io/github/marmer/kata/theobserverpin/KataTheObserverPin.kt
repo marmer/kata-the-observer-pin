61a45ed59dc2e7af165c48b419d9b23a28a3049a
mariano.mertinat <Mariano.Mertinat@gematik.de>
2023-01-20 10:06:19 +0100

    Initial commit

---
+++ src/main/kotlin/io/github/marmer/kata/theobserverpin/KataTheObserverPin.kt
@@ -0,0 +1,11 @@
+package io.github.marmer.kata.theobserverpin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KataTheObserverPinApplication
+
+fun main(args: Array<String>) {
+    runApplication<KataTheObserverPinApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/marmer/kata/theobserverpin/KataTheObserverPinTests.kt
@@ -0,0 +1,13 @@
+package io.github.marmer.kata.theobserverpin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KataTheObserverPinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
